1019b25b8c9a372b0f5ac0c3090fac8b86093bc1
Vaishali giri <giri.techhub23@gmail.com>
2020-07-21 00:54:45 +0530

    this is the first change by

---
+++ DemoProject/src/org/techhub/First.java
@@ -0,0 +1,15 @@
+package org.techhub;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str[]= {"ram@gmail.com,shyam@gmail.com"};
+		for(int i=0;i<str.length;i++)
+		{
+			System.out.println("Good Morning");
+		}
+	}
+
+}
